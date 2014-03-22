bb582720756eb39f937e62c20289b2a3d5374024
Laurent Van Winckel <laurentvw@gmail.com>
2014-03-22 19:35:04 +0100

    Convert indentation to spaces

--- src/Laurentvw/LavaCrawler/Matcher.php
+++ src/Laurentvw/LavaCrawler/Matcher.php
@@ -6,43 +6,43 @@ use \Symfony\Component\Translation\Translator;
 
 class Matcher {
 
-	/**
-	 * @var array
-	 */
-	protected $matches = array();
+    /**
+     * @var array
+     */
+    protected $matches = array();
 
-	/**
-	 * @var string
-	 */
-	protected $errors = '';
+    /**
+     * @var string
+     */
+    protected $errors = '';
 
-	/**
-	 * @var \Closure
-	 */
-	protected $filter = null;
+    /**
+     * @var \Closure
+     */
+    protected $filter = null;
 
-	/**
-	 * @var \Illuminate\Validation\Factory
-	 */
-	protected $validationFactory;
+    /**
+     * @var \Illuminate\Validation\Factory
+     */
+    protected $validationFactory;
 
-	/**
+    /**
      * Create a new Matcher instance.
      *
      * @param array $matches
      * @param \Closure $filter
      * @return void
      */
-	function __construct($matches = array(), $filter = null)
-	{
-		$this->setMatches($matches);
-		$this->setFilter($filter);
+    function __construct($matches = array(), $filter = null)
+    {
+        $this->setMatches($matches);
+        $this->setFilter($filter);
 
-		$translator = new Translator('en');
-		$this->validationFactory = new ValidationFactory($translator);
-	}
+        $translator = new Translator('en');
+        $this->validationFactory = new ValidationFactory($translator);
+    }
 
-	/**
+    /**
      * Set the matches
      *
      * @param array $matches
@@ -68,67 +68,67 @@ class Matcher {
         return $this;
     }
 
-	public function getErrors()
-	{
-		return $this->errors;
-	}
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 
-	/**
+    /**
      * Fetch the values from a match
      *
      * @param array $data
      * @param string $url
      * @return array
      */
-	public function fetch(array $data, $url = '')
-	{
-		$result = array();
+    public function fetch(array $data, $url = '')
+    {
+        $result = array();
 
-		$dataRules = array();
+        $dataRules = array();
 
-		foreach ($this->matches as $match)
-		{
-			// Get the match value, optionally apply a function to it
-			if (isset($match['apply']))
-			{
-				$result[$match['name']] = $match['apply']($data[$match['id']], $url);
-			}
-			else
-			{
-				$result[$match['name']] = $data[$match['id']];
-			}
+        foreach ($this->matches as $match)
+        {
+            // Get the match value, optionally apply a function to it
+            if (isset($match['apply']))
+            {
+                $result[$match['name']] = $match['apply']($data[$match['id']], $url);
+            }
+            else
+            {
+                $result[$match['name']] = $data[$match['id']];
+            }
 
-			// Get the validation rules for this match
-			if (isset($match['rules']))
-			{
-				$dataRules[$match['name']] = $match['rules'];
-			}
-		}
+            // Get the validation rules for this match
+            if (isset($match['rules']))
+            {
+                $dataRules[$match['name']] = $match['rules'];
+            }
+        }
 
-		// Validate the data
-		$validator = $this->validationFactory->make($result, $dataRules);
-		if ($validator->fails())
-		{
-			$this->errors .= 'Validation failed for: ' . "\r\n";
-			foreach ($validator->messages()->getMessages() as $name => $messages)
-			{
-				foreach ($messages as $message)
-				{
-					$this->errors .= var_export($result[$name], true) . ': ' . $message . "\r\n";
-				}
-			}
+        // Validate the data
+        $validator = $this->validationFactory->make($result, $dataRules);
+        if ($validator->fails())
+        {
+            $this->errors .= 'Validation failed for: ' . "\r\n";
+            foreach ($validator->messages()->getMessages() as $name => $messages)
+            {
+                foreach ($messages as $message)
+                {
+                    $this->errors .= var_export($result[$name], true) . ': ' . $message . "\r\n";
+                }
+            }
 
-			$this->errors .= "\r\n";
+            $this->errors .= "\r\n";
 
-			return false;
-		}
-		// Filter the data
-		elseif ($this->filter && ! call_user_func($this->filter, $result))
-		{
-			return false;
-		}
+            return false;
+        }
+        // Filter the data
+        elseif ($this->filter && ! call_user_func($this->filter, $result))
+        {
+            return false;
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 
 }
